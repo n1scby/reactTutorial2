0ac27071276c093f486c478d0d1467c113a0fc9a
Nic B <n1scby@yahoo.com>
2018-07-25 12:17:45 -0400

    + and -

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
+import List from './List';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isOn: true,
+      term: '',
+      items: [],
+      clicks: 0,
+      show: true
     };
 
     this.toggle = this.toggle.bind(this);
@@ -17,6 +22,33 @@ class App extends Component {
     this.setState({
       isOn: !this.state.isOn
     });
+  }
+
+
+  onChange = (event) => {
+    this.setState({term: event.target.value});
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: this.state.items.concat(this.state.term) //[...this.state.items, this.state.term]  this works the same as concat.  it is a spread attribute ...
+    })
+  }
+
+  incrementItem = () => {
+    this.setState({lcicks: this.state.clicks++ });
+  }
+
+  decreaseItem = () => {
+    this.setState({clicks: this.state.clicks-1});
+  }
+
+  toggleClick = () => {
+    this.setState({
+      show: !this.state.show
+    })
   }
 
   render() {
@@ -33,9 +65,26 @@ class App extends Component {
         <button onClick={this.toggle}>
           {this.state.isOn ? 'On' : 'Off'}
         </button>
+        <form className="App Form" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
+        <div>
+          <button onClick={this.incrementItem}>+ 1</button>
+          <button onClick={this.decreaseItem}>- 1</button>
+          <button onClick={this.toggleClick}>{this.state.show ? 'Hide Number' : 'Show Number'} </button>
+            <div>
+            {this.state.show ? <h2>{this.state.clicks}</h2> : ''}
+            </div>
+        </div>
       </div>
     );
   }
 }
+
+
+
+
 
 export default App;
